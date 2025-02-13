0093086f7b077312d3aea5a00512191b9e74d0ab
Daniele Picone <danielepicone97@gmail.com>
2025-02-13 11:34:12 +0100

    User9 completato

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -34,7 +35,16 @@ class ResizeImage implements ShouldQueue
         $srcPath = storage_path() . '/app/public/' . $this->path . '/' .$this->filename;
         $destpath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->filename;
         
-        Image::load($srcPath)->crop($w,$h, CropPosition::Center)->save($destpath);
+        Image::load($srcPath)
+        ->crop($w,$h, CropPosition::Center)
+        -> watermark(
+          base_path('resources/img/watermak.png'),
+          width: 50,
+          height: 50,
+          paddingX: 5,
+          paddingY: 5,
+          paddingUnit: Unit::Percent
+        )->save($destpath);
 
 
 
